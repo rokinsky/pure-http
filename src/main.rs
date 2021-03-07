e89ef05efcc94d822ebf377ab8e823228aa57242
Aleksander Sorokin <ankezy@gmail.com>
2021-03-07 23:50:17 +0300

    move logic of creating responses to handler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 
+use crate::website_handler::WebsiteHandler;
 use server::Server;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
